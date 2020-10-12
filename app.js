df24e57c7e039e1683b7ed380535f5d4f985b7dc
Cesar Preza <cpreza93@yahoo.com>
2020-10-11 23:23:59 -0700

    set value for 1 button

---
+++ app.js
@@ -0,0 +1,9 @@
+const calculatorDisplay = document.getElementById('calculator-display').value = '0';
+
+
+function oneKeyValue() {
+    const oneKey = document.getElementById('numberOne').value;
+    oneKey.addEventListener('click', function() {
+        
+    });
+}
