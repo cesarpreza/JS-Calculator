a810122029f2ce7e0f25805a907c28adfdc0429b
Cesar Preza <cpreza93@yahoo.com>
2020-10-16 23:03:47 -0700

    Number button functions

--- app.js
+++ app.js
@@ -1,10 +1,93 @@
 
 const display = document.getElementById('calculator-display');
 
+function numberZero() {
+    const zeroButton = document.getElementById('zero');
+    zeroButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
 function numberOne() {
-    oneButton.document.getElementById('one').value;
+    const oneButton = document.getElementById('one');
     oneButton.addEventListener('click', (e) => {
-        calculatorDisplay = e.target.parentElement;
-        console.log(calculatorDisplay);
+        display.value = e.target.value;
     });
 }
+function numberTwo() {
+    const twoButton = document.getElementById('two');
+    twoButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberThree() {
+    const threeButton = document.getElementById('three');
+    threeButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberFour() {
+    const fourButton = document.getElementById('four');
+    fourButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberFive() {
+    const fiveButton = document.getElementById('five');
+    fiveButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberSix() {
+    const sixButton = document.getElementById('six');
+    sixButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberSeven() {
+    const sevenButton = document.getElementById('seven');
+    sevenButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberEight() {
+    const eightButton = document.getElementById('eight');
+    eightButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberNine() {
+    const nineButton = document.getElementById('nine');
+    nineButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+function numberDecimal() {
+    const decimalButton = document.getElementById('decimal');
+    decimalButton.addEventListener('click', (e) => {
+        display.value = e.target.value;
+    })
+}
+
+
+
+
+numberZero();
+numberOne();
+numberTwo();
+numberThree();
+numberFour();
+numberFive();
+numberSix();
+numberSeven();
+numberEight();
+numberNine();
+numberDecimal();
