977e02ab8eeb6daafe4d69de929f1b0baecc5ba2
Cesar Preza <cpreza@scoi.com>
2020-10-26 16:33:28 -0700

    operator buttons functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ let firstNumber = null;
 let secondNumber = null;
 let operand1 = null;
 let calcResult = document.getElementById('equal');
+let multiply = document.getElementById('times');
+let division = document.getElementById('divide');
 
 function numberButtonClicked() {
     document.querySelectorAll('.number-button').forEach(item => {
@@ -11,23 +13,31 @@ function numberButtonClicked() {
             if(operand1 !== null) {
                 secondNumber = e.target.value;
                 display.value = secondNumber;
-                console.log(secondNumber); //! remove before submit
             } else {
                 firstNumber = e.target.value;
                 display.value = firstNumber;
-                console.log(firstNumber); //! remove before submit
             }
         })
     })
 };
 
 function result() {
-    calcResult.addEventListener('click', (e) => {
-        if(numberButtonClicked) {
-            calcResult = e.target.value;
-            //! display.value should show number + number in display. 
+    calcResult.addEventListener('click', function() {
+        switch(operand1) {
+            case '+':
+                display.value = parseInt(firstNumber) + parseInt(secondNumber);
+                console.log(display.value);
+                break;
+            case '-':
+                display.value = parseInt(firstNumber) - parseInt(secondNumber);   
+                break;
+            case multiply.value:
+                display.value = parseInt(firstNumber) * parseInt(secondNumber); 
+                break;
+            case division.value:
+                display.value = parseInt(firstNumber) / parseInt(secondNumber);    
         }
-    })
+})
 }
 
 function allClear() {
@@ -47,7 +57,6 @@ function operatorClicked() {
     operator.forEach(item => {
         item.addEventListener('click', (e) => {
             operand1 = e.target.value;
-            console.log(operand1); //! remove before submit
         })
     })
 }
@@ -56,96 +65,3 @@ result()
 operatorClicked();
 allClear();
 numberButtonClicked();
-
-
-// function numberZero() {
-//     const zeroButton = document.getElementById('zero');
-//     zeroButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     });
-// }
-
-// function numberOne() {
-//     const oneButton = document.getElementById('one');
-//     oneButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     });
-// }
-// function numberTwo() {
-//     const twoButton = document.getElementById('two');
-//     twoButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberThree() {
-//     const threeButton = document.getElementById('three');
-//     threeButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberFour() {
-//     const fourButton = document.getElementById('four');
-//     fourButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberFive() {
-//     const fiveButton = document.getElementById('five');
-//     fiveButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberSix() {
-//     const sixButton = document.getElementById('six');
-//     sixButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberSeven() {
-//     const sevenButton = document.getElementById('seven');
-//     sevenButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberEight() {
-//     const eightButton = document.getElementById('eight');
-//     eightButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberNine() {
-//     const nineButton = document.getElementById('nine');
-//     nineButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-// function numberDecimal() {
-//     const decimalButton = document.getElementById('decimal');
-//     decimalButton.addEventListener('click', (e) => {
-//         display.value = e.target.value;
-//     })
-// }
-
-
-
-
-
-// numberZero();
-// numberOne();
-// numberTwo();
-// numberThree();
-// numberFour();
-// numberFive();
-// numberSix();
-// numberSeven();
-// numberEight();
-// numberNine();
-// numberDecimal();
